26a9ca78c8f73f0184cc0f33766aeb6e2838157d
Bruno Medeiros <brunorafamed.com@gmail.com>
2026-03-04 22:21:59 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Headers from "./pages/header.js"
+// import Sidebar from "./componentes/Sidebar.jsx"
+
+function App() {
+  return (
+    <div>
+      <Headers />
+      {/* <Sidebar /> */}
+    </div>
+  );
+}
+
+export default App;
+
